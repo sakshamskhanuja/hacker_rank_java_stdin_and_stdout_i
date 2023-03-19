ca419bf654783f39e0e5990a7f2fdf03b24fa4b1
Saksham Singh Khanuja <sskhanuja94@gmail.com>
2023-03-19 16:50:24 +0530

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Scans the user input.
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            // Scans three whole numbers.
+            int a = Integer.parseInt(scanner.nextLine());
+            int b = Integer.parseInt(scanner.nextLine());
+            int c = Integer.parseInt(scanner.nextLine());
+
+            // Prints entered numbers.
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(c);
+        } catch (NumberFormatException e) {
+            System.out.println("Enter whole number.");
+        }
+    }
+}
